cc328dad62e4d4fe53304c4769b73eb5b08b93fc
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-11 20:43:31 +0300

    scx35-common: add sensors HIDL service

--- hidl.mk
+++ hidl.mk
@@ -50,7 +50,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-	android.hardware.sensors@1.0-impl
+	android.hardware.sensors@1.0-impl \
+	android.hardware.sensors@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
