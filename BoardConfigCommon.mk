a46f4bf5c01f8302b787895df43d368ff337660f
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-18 00:21:15 +0300

    scx35-common: preopt system applications
    Precompile system applications in addition to the boot image and
    system server.
    
    This reduces first-boot compilation work and improves initial application
    launch performance while keeping the system image within the partition
    limit.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,7 +108,7 @@ BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
 
 # Enable dex-preoptimization to speed up first boot sequence
 WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 PRODUCT_DEX_PREOPT_BOOT_FLAGS += --compiler-filter=speed
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
 WITH_DEX_PREOPT_GENERATE_PROFILE := false
