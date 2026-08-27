a4c468fad07b1bae4b5738ee2a36072fc85c9228
Ma3a_XaTa <v.radulevich@gmail.com>
2026-08-27 21:48:35 +0300

    scx35-common: stop advertising camera flash

--- common.mk
+++ common.mk
@@ -192,7 +192,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Permissions
 PERMISSIONS_XML_FILES := \
-	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
